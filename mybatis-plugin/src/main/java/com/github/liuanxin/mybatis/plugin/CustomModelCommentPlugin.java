11aead4fa11d8d0ca7f46d060ace15c3c8162707
liuanxin <liujie@wolianw.com>
2017-12-14 14:52:43 +0800

    fix

--- mybatis-plugin/src/main/java/com/github/liuanxin/mybatis/plugin/CustomModelCommentPlugin.java
+++ mybatis-plugin/src/main/java/com/github/liuanxin/mybatis/plugin/CustomModelCommentPlugin.java
@@ -7,6 +7,7 @@ import org.mybatis.generator.api.PluginAdapter;
 import org.mybatis.generator.api.dom.java.Field;
 import org.mybatis.generator.api.dom.java.TopLevelClass;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -65,15 +66,14 @@ public class CustomModelCommentPlugin extends PluginAdapter {
                 f.getJavaDocLines().clear();
                 String remarks = column.getRemarks();
                 String columnName = column.getActualColumnName();
-                /*
-                if ("java.util.Date".equalsIgnoreCase(column.getFullyQualifiedJavaType().toString())) {
+                String columnType = column.getFullyQualifiedJavaType().toString();
+                if (Arrays.asList("java.util.Date", "java.sql.Time", "java.sql.Date").contains(columnType)) {
                     String propertyName = underlineToCamel(columnName);
-                    f.addJavaDocLine(String.format("/** 为 \"%s\" 提供查询的起始值 * /", columnName));
+                    f.addJavaDocLine(String.format("/** 为 \"%s\" 提供查询的起始值 */", columnName));
                     f.addJavaDocLine(String.format("private Date %sStart;", propertyName));
-                    f.addJavaDocLine(String.format("/** 为 \"%s\" 提供查询的结束值 * /", columnName));
+                    f.addJavaDocLine(String.format("/** 为 \"%s\" 提供查询的结束值 */", columnName));
                     f.addJavaDocLine(String.format("private Date %sEnd;", propertyName));
                 }
-                */
                 if (remarks != null) {
                     remarks = remarks.trim();
                     if (remarks.length() > 0) {
@@ -95,7 +95,8 @@ public class CustomModelCommentPlugin extends PluginAdapter {
         for (int i = 0; i < len; i++) {
             char c = param.charAt(i);
             if (c == '_' || c == '-') {
-                if (++i < len) {
+                i++;
+                if (i < len) {
                     sb.append(Character.toUpperCase(param.charAt(i)));
                 }
             } else {
