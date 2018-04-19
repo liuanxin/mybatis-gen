67c4812ee0de52c345a69b8dc7a17d9f65d2ef15
liujie <liujie@wolianw.com>
2018-04-19 17:53:45 +0800

    fix

--- mybatis-plugin/src/main/java/com/github/liuanxin/mybatis/plugin/CustomModelCommentPlugin.java
+++ mybatis-plugin/src/main/java/com/github/liuanxin/mybatis/plugin/CustomModelCommentPlugin.java
@@ -7,7 +7,6 @@ import org.mybatis.generator.api.PluginAdapter;
 import org.mybatis.generator.api.dom.java.Field;
 import org.mybatis.generator.api.dom.java.TopLevelClass;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -66,14 +65,16 @@ public class CustomModelCommentPlugin extends PluginAdapter {
                 f.getJavaDocLines().clear();
                 String remarks = column.getRemarks();
                 String columnName = column.getActualColumnName();
+                /*
                 String columnType = column.getFullyQualifiedJavaType().toString();
                 if (Arrays.asList("java.util.Date", "java.sql.Time", "java.sql.Date").contains(columnType)) {
                     String propertyName = underlineToCamel(columnName);
-                    f.addJavaDocLine(String.format("/** 为 \"%s\" 提供查询的起始值 */", columnName));
+                    f.addJavaDocLine(String.format("/** 为 \"%s\" 提供查询的起始值 *\/", columnName));
                     f.addJavaDocLine(String.format("private Date %sStart;", propertyName));
-                    f.addJavaDocLine(String.format("/** 为 \"%s\" 提供查询的结束值 */", columnName));
+                    f.addJavaDocLine(String.format("/** 为 \"%s\" 提供查询的结束值 *\/", columnName));
                     f.addJavaDocLine(String.format("private Date %sEnd;", propertyName));
                 }
+                */
                 if (remarks != null) {
                     remarks = remarks.trim();
                     if (remarks.length() > 0) {
