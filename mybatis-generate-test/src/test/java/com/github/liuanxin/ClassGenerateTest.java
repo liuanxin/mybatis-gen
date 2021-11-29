4869f9669615c0252ee66bbe378cf768ee8bbfc9
Jie <zt17947@ztn.com>
2021-11-29 09:52:24 +0800

    fix

--- mybatis-generate-test/src/test/java/com/github/liuanxin/ClassGenerateTest.java
+++ mybatis-generate-test/src/test/java/com/github/liuanxin/ClassGenerateTest.java
@@ -81,7 +81,7 @@ public class ClassGenerateTest extends AbstractTransactionalJUnit4SpringContextT
     /** true 表示生成 feign 的 req 和 res 文件 */
     private static final boolean GENERATE_FEIGN = false;
 
-    /** 是否把 tinyint(1) 映射成 Boolean */
+    /** 是否把 tinyint(1) 映射成 Boolean. mysql 8.0.17 的版本 tinyint int bigint 设置长度将无效 */
     private static final boolean TINYINT1_TO_BOOLEAN = false;
 
     // 上面是配置项, 下面的不用了
@@ -227,15 +227,24 @@ public class ClassGenerateTest extends AbstractTransactionalJUnit4SpringContextT
 
     private static void deleteDirectory(File dir) {
         if (dir.exists()) {
-            File[] files = dir.listFiles();
-            if (files != null) {
-                for (File file : files) {
-                    deleteDirectory(file);
+            if (dir.isDirectory()) {
+                File[] files = dir.listFiles();
+                if (files != null && files.length > 0) {
+                    for (File file : files) {
+                        deleteDirectory(file);
+                    }
                 }
-            }
-            boolean flag = dir.delete();
-            if (!flag) {
-                throw new RuntimeException(String.format("文件(%s)删除失败", dir));
+                boolean flag = dir.delete();
+                if (flag) {
+                    System.out.printf("文件夹(%s)删除成功\n", dir);
+                }
+            } else {
+                boolean flag = dir.delete();
+                if (flag) {
+                    System.out.printf("文件(%s)删除成功\n", dir);
+                } else {
+                    throw new RuntimeException(String.format("文件(%s)删除失败", dir));
+                }
             }
         }
     }
